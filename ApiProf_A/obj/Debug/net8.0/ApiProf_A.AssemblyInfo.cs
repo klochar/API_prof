ed1e7d17aa0b4e4a15aa3824df4b4e4a37e14b92
klochar <minayouhanna@outlook.com>
2024-07-10 08:54:36 -0400

    ajout du nom de database dans le script creation ADuser windows server

--- ApiProf_A/obj/Debug/net8.0/ApiProf_A.AssemblyInfo.cs
+++ ApiProf_A/obj/Debug/net8.0/ApiProf_A.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiProf_A")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+695283e265f76c61716a45d61aed3d56134c6203")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e230c8a2b01bb810bc4b95ca962d9ff1d593f9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiProf_A")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiProf_A")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
